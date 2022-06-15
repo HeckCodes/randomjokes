a8b8159a5b5316a448638e0bb1921688dfabdf75
HeckCodes <chandramaulishastri@gmail.com>
2022-06-15 21:58:18 +0530

    Basic HomeScreen Layout

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:randomjokes/colors.dart';
+import 'package:randomjokes/fonts.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -8,8 +11,41 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  final RandomColors randomColors = RandomColors();
+  final RandomFonts randomFonts = RandomFonts();
+
+  late Color randomColor;
+  late TextStyle randomTextStyle;
+
+  @override
+  void initState() {
+    randomColor = randomColors.randomColor;
+    randomTextStyle = randomFonts.randomTextStyle;
+    super.initState();
+  }
+
+  void changeColor() {
+    setState(() {
+      randomColor = randomColors.randomColor;
+    });
+  }
+
+  void changeTextStyle() {
+    setState(() {
+      randomTextStyle = randomFonts.randomTextStyle;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return AnnotatedRegion(
+      value: SystemUiOverlayStyle(
+        statusBarColor: randomColor,
+        systemNavigationBarColor: randomColor,
+      ),
+      child: Scaffold(
+        backgroundColor: randomColor,
+      ),
+    );
   }
 }
